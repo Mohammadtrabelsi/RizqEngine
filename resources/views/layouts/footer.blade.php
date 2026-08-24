038891f22f8709d0ad3951b4e4d4abd9f62c167d
Mohammad Trabelsi <mohammad.trabelsi@gmail.com>
2026-08-24 09:31:15 +0100

    version

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="c-footer">
     <div>Triangle Pos © {{ date('Y') }} || {{ __('app.developed-by') }} <strong><a target="_blank" href="https://fahimanzam.netlify.app">Fahim Anzam Dip</a></strong> / {{ __('app.developed') }} <strong>Mohammad TRABELSI</strong></div>
 
-    <div class="mfs-auto d-md-down-none">{{ __('app.version') }} <strong class="text-danger">1.0</strong></div>
+    <div class="mfs-auto d-md-down-none">{{ __('app.version') }} <strong class="text-danger">v3.4.0</strong></div>
 </footer>
